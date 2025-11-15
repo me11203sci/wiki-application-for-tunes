bc463078020acc0c98302a1c67204177e4bdf91f
Melesio Albavera <ma6hv@umsystem.edu>
2025-11-15 13:40:17 -0600

    Split README contribution section into separate CONTRIBUTION.md
    Additionally, added precommit hook to preform formatting/testing checks
    prior to a commit.

--- src/main.py
+++ src/main.py
@@ -1,7 +1,20 @@
-"""Entry point for the `waft` application.
+"""Execution entry point for the `waft` application.
 
 Todo.
 """
+# from textual.app import App
+
+
+class GameApp:
+    """Hi."""
+
+    pass
+
+
+def silly():
+    """Hi."""
+    pass
+
 
 if __name__ == "__main__":
     pass
